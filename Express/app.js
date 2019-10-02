a029c52f77ae4fecd3ff57b746557a9d638698ea
Kian Nikzad <kian.nikzad@gmail.com>
2019-10-02 12:27:59 -0700

    Initial Commit

---
+++ Express/app.js
@@ -0,0 +1,5 @@
+var express = require('express');
+
+const app = express();
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(4000, () => console.log(`Express server running on port 4000`));
