301f878c433b265579fc14283e459dbf1e13d8d6
Oliver Melgrove <38347766+melgrove@users.noreply.github.com>
2020-04-20 23:18:47 -0700

    Added cors package and cors handling to express

--- Express/app.js
+++ Express/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const query = require('./query.js');
 const app = express();
+const cors = require('cors');
 const port = 4001;
 
 //`npm run dev` runs a nodemon server for development
 //`node ~/app.js` runs a node server
 
-app.get('/toilet', query.firstResponse); 
+app.get('/toilet', cors(), query.firstResponse); 
 
 app.listen(port, () => console.log(`Express server running on port ${port}`));
