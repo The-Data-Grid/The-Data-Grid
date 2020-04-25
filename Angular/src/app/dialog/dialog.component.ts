6a932c6602785ee409cdf3a55ecbc3bd60185b47
Ryan Jackson <10ryanjackson10@gmail.com>
2020-04-24 19:10:19 -0700

    log in popup

--- Angular/src/app/dialog/dialog.component.ts
+++ Angular/src/app/dialog/dialog.component.ts
@@ -7,10 +7,36 @@ import { MatDialogRef } from "@angular/material/dialog";
 })
 export class DialogComponent implements OnInit {
 
-  constructor(public dialogRef: MatDialogRef<DialogComponent>) { }
+  formsFilledOut = false;
+
+  handleInput() {
+    console.log("help");
+  }
+
+
+  checkForms() {
+    var user = (<HTMLInputElement>document.getElementById("username")).value;
+    var pass = (<HTMLInputElement>document.getElementById("password")).value;
+
+    if (user.length != 0 && pass.length!=0) {
+      this.formsFilledOut = true;
+    }
+    else {
+      this.formsFilledOut = false;
+    }
+
+    console.log(this.formsFilledOut);
+
+  }
+  
+
+  constructor(public dialogRef: MatDialogRef<DialogComponent>) { 
+  }
 
   ngOnInit() {
+    this.dialogRef.updateSize('30%', '85%')
   }
+
 
 
   close() {
