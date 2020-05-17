834eec05d0147c83b5b7472332cae76ef9e45fe7
melgrove <38347766+melgrove@users.noreply.github.com>
2020-05-17 01:08:36 -0700

    added to insert.js

--- Express/insert.js
+++ Express/insert.js
@@ -1,5 +1,10 @@
 //statements here
+"INSERT INTO loc (location_id, room_id) \
+VALUES (DEFAULT, 2929); -- location_id is a SERIAL column"
 
+"INSERT INTO audit_toilet (observation_id, gpf, date_conducted, location_id, commentary) \
+VALUES (DEFAULT, 32, 'date', currval('loc_location_id_seq'), 'commentary');"
+ //current value of SERIAL sequence in loc.location_id
 
 
 
