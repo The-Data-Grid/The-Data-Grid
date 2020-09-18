9c10717d59a2b669f87df7ca4d78fc14d00e9b9e
Tanya Zhong <tanyazhong1@gmail.com>
2020-09-18 14:48:52 -0700

    audit upload

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { IndexComponent } from './index/index.component';
 import { UploadComponent } from './upload/upload.component';
 import { AboutComponent } from './about/about.component';
 import { UploadFilesComponent } from './upload-files/upload-files.component';
+import { UploadAuditComponent } from './upload-audit/upload-audit.component';
 import { DownloadComponent } from './download/download.component'
 import { ProfilePageComponent } from './profile-page/profile-page.component'
 
@@ -15,6 +16,7 @@ const routes: Routes = [
   { path:'upload', component:UploadComponent },
   { path:'about', component:AboutComponent },
   { path: 'upload-files', component: UploadFilesComponent },
+  { path: 'upload-audit', component: UploadAuditComponent },
   { path: 'download', component: DownloadComponent },
   { path: 'profile-page', component: ProfilePageComponent },
 
--- Angular/src/app/upload-audit/upload-audit.component.ts
+++ Angular/src/app/upload-audit/upload-audit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-upload-audit',
@@ -9,7 +10,15 @@ export class UploadAuditComponent implements OnInit {
 
   constructor() { }
 
+  dataSource = new MatTableDataSource();
+  tempData = [
+    { auditName: "Bathroom Audit 1", uploadStatus: "uploaded", _id: "1234567890" },
+    { auditName: "Bathroom Audit 2", uploadStatus: "uploaded", _id: "5555555555" }
+  ]
+  displayedColumns: string[] = ['auditName', 'uploadStatus'];
+
   ngOnInit(): void {
+    this.dataSource.data = this.tempData;
   }
 
 }
