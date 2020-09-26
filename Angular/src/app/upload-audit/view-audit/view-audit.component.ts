ee8faefb1a91285e10bf837d5b4194f10c4373a2
Ryan Jackson <10ryanjackson10@gmail.com>
2020-09-26 14:20:41 -0700

    root features dialog

--- Angular/src/app/upload-audit/view-audit/view-audit.component.ts
+++ Angular/src/app/upload-audit/view-audit/view-audit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { RootFeaturesComponent } from '../root-features/root-features.component';
 
 @Component({
   selector: 'app-view-audit',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewAuditComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   audit = { auditName: "Bathroom Audit 1", uploadStatus: "uploaded", _id: "1234567890" };
+
+
+  audits = [
+    {name:"Restroom Audit", features: ["Toilet", "Sink", "Urinal"]},
+    {name: "Irrigation Audit", features: ["Sprinkler", "Canal", "Sewer"]},
+    {name: "Boiler Room Audit", features: ["Boiler", "Pump"]}
+  ]
+
+
+  openRootFeatures(): void {
+    const dialogRef = this.dialog.open(RootFeaturesComponent, {
+      width: '400px',
+      height: '600px',
+      data: this.audits
+    })
+  }
   
   ngOnInit(): void {
   }
