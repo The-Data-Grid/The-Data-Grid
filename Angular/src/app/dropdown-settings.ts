f352bc802918bf2566ce6c2252fb8a3836c0975a
Tanya Zhong <tanyazhong1@gmail.com>
2020-10-25 12:54:19 -0700

    changed fake dropdown data

--- Angular/src/app/dropdown-settings.ts
+++ Angular/src/app/dropdown-settings.ts
@@ -20,9 +20,9 @@ export let SearchableChecklistDropdownSettings: IDropdownSettings = {
     allowSearchFilter: true
 };
 export let FakeData = [
-    { item_id: 1, item_text: 'Mumbai' },
-    { item_id: 2, item_text: 'Bangaluru' },
-    { item_id: 3, item_text: 'Pune' },
-    { item_id: 4, item_text: 'Navsari' },
-    { item_id: 5, item_text: 'New Delhi' }
+    { item_id: 1, item_text: 'BHS' },
+    { item_id: 2, item_text: 'REA' },
+    { item_id: 3, item_text: 'Clean Consulting' },
+    { item_id: 4, item_text: 'TGIF' },
+    { item_id: 5, item_text: 'SAR' }
   ];
