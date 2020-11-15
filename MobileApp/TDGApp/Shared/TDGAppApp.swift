6911b55f1d7dda6180d9ce85474010e488302d37
Diya Baliga <diyabaliga@diyas-mbp.lan>
2020-11-15 01:30:10 -0800

    xcode project created

---
+++ MobileApp/TDGApp/Shared/TDGAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TDGAppApp.swift
+//  Shared
+//
+//  Created by Diya Baliga on 11/15/20.
+//
+
+import SwiftUI
+
+@main
+struct TDGAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
