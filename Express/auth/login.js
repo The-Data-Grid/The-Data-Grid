b1c769c15888dff799011eb5590a8971a60bb24d
melgrove <oliver@melgrove.com>
2020-12-05 19:22:34 -0800

    fix db con in login

--- Express/auth/login.js
+++ Express/auth/login.js
@@ -3,9 +3,11 @@ const express = require('express');
 const app = express(); 
 const session = require('express-session'); 
 
-const postgresClient = require('./db/pg.js'); 
-const db = postgresClient.connect('main'); 
-//const util = require
+const {postgresClient} = require('../db/pg.js'); 
+// get connection object
+const db = postgresClient.getConnection.db
+// get SQL formatter
+const formatSQL = postgresClient.format;
 
 // session store init
 let Store = require('memorystore')(session); 
@@ -25,7 +27,10 @@ app.use(session({
 app.post('/login/', (req, res) => {
     let combo = [req.body.user, req.body.pass];
 
-    db.one('SELECT * FROM users WHERE name = $1 AND password = $2', req.body.user, req.body.pass)
+    db.one(formatSQL('SELECT item_id FROM item_users WHERE name = $(user) AND password = $(password)', {
+        user: combo[0], 
+        password: combo[1]
+    }))
     .then(data => {
         console.log('DATA:', data); 
         req.session.loggedIn = true;
@@ -34,7 +39,7 @@ app.post('/login/', (req, res) => {
     })
     .catch(error => {
         console.log('ERROR:', error);
-        req.session.destroy(); 
+        // req.session.destroy(); 
         res.send('not a valid login');
     })
 });
