8d13dc7664ec56c766660bcd541da83970dc12e2
Diya Baliga <diyabaliga@diyas-mbp.lan>
2021-02-28 07:47:22 -0800

    audit selection page and root feature selection implemented, home bar design changed

--- MobileApp/mobileTDG/mobileTDG/tabView.swift
+++ MobileApp/mobileTDG/mobileTDG/tabView.swift
@@ -21,34 +21,45 @@ struct tabView: View {
                     homeView()
                 case .settings:
                     settingsView()
-                case .submitted:
-                    submittedAuditsView()
-                case .unsubmitted:
-                    unsubmittedAuditsView()
+                case .audits:
+                    auditsView()
                 case .detail:
                     auditDetailView()
                 }
                 
                 Spacer()
                 HStack {
+                    Image(systemName: "gear")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .padding(20)
+                        .frame(width: geometry.size.width/3,
+                               height: viewRouter.currentTab != .settings ? 70: 75)
+                        .foregroundColor(.white)
+                        .onTapGesture {
+                            viewRouter.currentTab = .settings
+                        }
                     Image(systemName: "house")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .padding(20)
-                        .frame(width: geometry.size.width/3, height:70)
-                        .foregroundColor(viewRouter.currentTab != .home ? .black : .gray)
+                        .frame(width: geometry.size.width/3,
+                               height: viewRouter.currentTab != .home ? 70: 75)
+                        .foregroundColor(.white)
                         .onTapGesture {
                             viewRouter.currentTab = .home
                         }
                     Image(systemName: "phone.fill.arrow.up.right").resizable()
                         .aspectRatio(contentMode: .fit)
                         .padding(20)
-                        .frame(width: geometry.size.width/3, height:70)
+                        .frame(width: geometry.size.width/3,
+                               height: 65)
+                        .foregroundColor(.white)
                         .onTapGesture {
                             isCommunicating = true
                         }
                 }.frame(width: geometry.size.width, height: geometry.size.height/11)
-                .background(Color.white.shadow(radius: 2))
+                .background(Color("green").opacity(0.80).shadow(radius: 2))
                 .sheet(isPresented: $isCommunicating, content: {
                     communicationView()
                 })
@@ -59,6 +70,6 @@ struct tabView: View {
 
 struct TabView_Previews: PreviewProvider {
     static var previews: some View {
-        tabView().environmentObject(ViewRouter())
+        tabView().environmentObject(ViewRouter()).environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
 }
