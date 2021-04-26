b13f61193cfcc4c619936f1f5a55d1be774e3c73
melgrove <38347766+melgrove@users.noreply.github.com>
2021-04-26 04:21:07 -0700

    Add default password handling for postgres client

--- Express/db/init.sh
+++ Express/db/init.sh
@@ -5,5 +5,5 @@ eval PGPASSWORD=postgres psql -U postgres -d postgres -h localhost -p 5432 -f ".
 echo "Dropped and Created"
 eval PGPASSWORD=postgres psql -U postgres -d v5 -h localhost -p 5432 -f "./PostgreSQL/V5.0.sql"
 echo "Run V5.0"
-eval npm run construct -- make-schema water
+eval PGPASSWORD=postgres npm run construct -- make-schema water
 echo "Constructed Schema"
