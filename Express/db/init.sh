c782c6995371ee9ddc520a6ff78b0afa37dcc35f
melgrove <38347766+melgrove@users.noreply.github.com>
2021-04-26 03:32:33 -0700

    small init addition for github workflow

--- Express/db/init.sh
+++ Express/db/init.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 echo "Init db"
-eval psql -U postgres -d postgres -f "./PostgreSQL/reset.sql"
+eval PGPASSWORD=postgres psql -U postgres -d postgres -f "./PostgreSQL/reset.sql"
 echo "Dropped and Created"
 eval psql -U postgres -d v5 -f "./PostgreSQL/V5.0.sql"
 echo "Run V5.0"
