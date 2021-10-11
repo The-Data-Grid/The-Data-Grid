4a70979ab23e65b8ff1e02c9004ff5a6038639e3
David Nguyen <dxvidnguyen@gmail.com>
2021-10-11 15:59:26 -0700

    add alert for when when incorrect user authorization

--- MobileApp/mobileTDG/mobileTDG/loginView.swift
+++ MobileApp/mobileTDG/mobileTDG/loginView.swift
@@ -10,20 +10,16 @@ import Foundation
 
 class loginViewModel: ObservableObject {
    // userInput + environment variables
-   let cont: () -> Void
    @Published var username = ""
    @Published var password = ""
+   @Published var wrongAuth = false
    @Published var userAuth = false {
       didSet{
          if userAuth == true {
             UserDefaults.standard.set(username, forKey: "username")
             UserDefaults.standard.set(true, forKey: "loggedIn")
-            cont()
          }
       }
-   }
-   init(c: @escaping () -> Void){
-      self.cont = c
    }
 }
 
@@ -33,14 +29,6 @@ struct loginView: View {
    @EnvironmentObject var viewRouter: ViewRouter
    @ObservedObject var loginObject : loginViewModel
 
-   static func setup(){
-      loginObject = loginViewModel(c:login)
-   }
-
-   init(){
-      self.loginObject = loginViewModel(c:self.login)
-   }
-
    func login() {
       viewRouter.currentPage = .tab
    }
@@ -49,7 +37,6 @@ struct loginView: View {
       var check = "none"
       //preparing the url
       let url = URL(string: "https://thedatagrid.org/api/login")
-      // let url = URL(string: "https://jsonplaceholder.typicode.com/todos")
       guard let requestUrl = url else { fatalError() }
 
       // preparing the request object
@@ -73,14 +60,15 @@ struct loginView: View {
 
               // convert the data to string
               if let data = data, let dataString = String(data: data, encoding: .utf8) {
-                  print("Response data string:\n \(dataString)")
                   check = dataString
-                  print(check)
+                  // check if message is wrong
                   if check == "password matched and you logged in"{
                      loginObject.userAuth = true
+
                   }
                   else {
-                     print("bad")
+                     // wrong password combination
+                     loginObject.wrongAuth  = true
                   }
               }
       }
@@ -102,11 +90,11 @@ struct loginView: View {
                   .textFieldStyle(RoundedBorderTextFieldStyle())
                   .font(Font.custom("IBMPlexSans", size: 23, relativeTo: Font.TextStyle.body))
                   .autocapitalization(.none)
-               TextField("Password", text: $loginObject.password)
+
+               SecureField("Password", text: $loginObject.password)
                   .textFieldStyle(RoundedBorderTextFieldStyle())
                   .font(Font.custom("IBMPlexSans", size: 23, relativeTo: Font.TextStyle.body))
                   .autocapitalization(.none)
-                //SecureField("Password", text: $password).textFieldStyle(RoundedBorderTextFieldStyle()).font(Font.custom("IBMPlexSans", size: 23, relativeTo: Font.TextStyle.body))
                 
                 // Navigates to home page if user is Authorized
                 Button(action: {
@@ -114,8 +102,22 @@ struct loginView: View {
                 }) {
                     submitButtonContent()
                 }
+
+               // attempt to put in alert
+                .alert(isPresented: $loginObject.wrongAuth) {
+                  Alert(title: Text("Login Failed"),
+                        message: Text("The username or password you entered was incorrect. Please try again."),
+                        dismissButton: .default(Text("OK")))
+                }
+
+
                 Spacer()
-            }.padding()
+            }.padding().onChange(of: loginObject.userAuth, perform: { value in
+               if value {
+                  login()
+               }
+
+            })
     }
 }
 
@@ -124,7 +126,7 @@ struct loginView: View {
 // Canvas preview setup
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
-        loginView().environmentObject(ViewRouter())
+        loginView(loginObject: loginViewModel()).environmentObject(ViewRouter())
     }
 }
 
@@ -144,49 +146,3 @@ struct submitButtonContent: View {
 }
 
 
-
-func checkUser(name: String, word: String) -> String{
-   //preparing the url
-   let url = URL(string: "https://thedatagrid.org/api/login")
-   // let url = URL(string: "https://jsonplaceholder.typicode.com/todos")
-   guard let requestUrl = url else { fatalError() }
-
-   // preparing the request object
-   var request = URLRequest(url: requestUrl)
-   request.httpMethod = "POST"
-
-   // request parameters - is sent to the request body
-   // let postString = "hello=300&title=My urgent task&completed=false";
-   let postString = "email="+name+"&"+"pass="+word;
-   let test = postString
-
-   // http request body
-   request.httpBody = postString.data(using: String.Encoding.utf8);
-
-   // the http request
-   let task = URLSession.shared.dataTask(with: request)  { (data, response, error) in
-
-           // checking for error
-           if let error = error {
-               print("Error took place \(error)")
-               return
-           }
-
-           // convert the data to string
-           if let data = data, let dataString = String(data: data, encoding: .utf8) {
-               print("Response data string:\n \(dataString)")
-               // return dataString!
-               print("datastring \(dataString)")
-               print("test \(test)")
-               let test = dataString
-               //return test
-               print("test \(test)")
-           }
-   }
-
-   // return test
-   print("test \(test)")
-   task.resume()
-   print("test \(test)")
-   return test
-}
--- MobileApp/mobileTDG/mobileTDG/throwaway.swift
+++
@@ -1,8 +0,0 @@
-//
-//  throwaway.swift
-//  mobileTDG
-//
-//  Created by David Trung Nguyen on 8/25/21.
-//
-
-import Foundation
