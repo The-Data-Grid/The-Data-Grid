2ce3afd9423d7494a124ead5f21f1c175f54580d
ybshah02 <65425667+ybshah02@users.noreply.github.com>
2021-12-19 15:26:16 -0600

    starting spreadsheet

---
+++ Express/spreadsheet/generate.js
@@ -0,0 +1,9 @@
+// middleware req res get ordID, isItem, feature
+
+// metadata setup
+function setup () {}
+
+// format and generate spreadsheet
+function generate (frontendName, information, returnableID, colId, isNullable) {
+    // return res.sendFile(...)
+}
